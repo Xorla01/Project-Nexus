b888eb8f4a49f1654ddaa46debb32a9aef450daf
Richmond Cowther <richmondcowther@gmail.com>
2025-12-01 01:22:37 +0000

    feat: integrate movie API for fetching data

--- movie-recommendation-app/app/types/Movie.ts
+++ movie-recommendation-app/app/types/Movie.ts
@@ -24,6 +24,7 @@ export interface MovieResponse {
 
 export interface MovieCardProps {
   movie: Movie;
+  onFavoriteChange: (movie: Movie, isFavorite: boolean) => void;
 }
 
 export interface PageProps {
